a4d1cf7b6928a4060b1a85d2a5ed0646aa3fd2ab
Pablo Gonzales <gonzalescpablo@gmail.com>
2020-07-24 17:13:26 -0400

    styled room menu

--- src/components/RoomOptions/RoomMenu.tsx
+++ src/components/RoomOptions/RoomMenu.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { styled } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
 import { Link } from 'react-router-dom';
+import './RoomMenu.css';
 
 import useHeight from '../../hooks/useHeight/useHeight';
 
@@ -14,8 +16,17 @@ export default function App() {
 
   return (
     <Container style={{ height }}>
-      <Link to="/create">Crear Sala</Link>
-      <Link to="/join">Unirse a Sala</Link>
+      <div className="room-menu-options">
+        <Button variant="contained" color="primary">
+          Crear Sala
+        </Button>
+
+        <Link to="/join">
+          <Button variant="contained" color="primary">
+            Unirse a Sala
+          </Button>
+        </Link>
+      </div>
     </Container>
   );
 }
